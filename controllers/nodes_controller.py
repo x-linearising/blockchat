070ff17c95d3929e88dadff666ec432fec9f6605
Lazaros Athanasiadis <lathanasiadis.dsml@gmail.com>
2024-02-20 22:01:29 +0200

    rewrite done, bootstrap is now a separate class

--- controllers/nodes_controller.py
+++ controllers/nodes_controller.py
@@ -2,65 +2,98 @@ import logging
 
 from flask import Blueprint, abort, request
 
-from node import Node
-from node_memory import NodeMemory
+from node import Node, Bootstrap
 from request_classes.join_request import JoinRequest
 from response_classes.join_response import JoinResponse
+from constants import Constants 
 
 
-class NodesController:
-    """
-        A controller containing all the endpoints related to the Node resource.
-        Should only be used for the bootstrapping phase as the network is permanent
-        after that.
-    """
-    nodes_blueprint = Blueprint('nodes', __name__)
-    nodes_counter = 1
-    is_bootstrapping_phase_over = False
-    max_number_of_nodes = 10
+class BootstrapController:
+    
+    def __init__(self):
+        self.bootstrap = Bootstrap(Constants.BOOTSTRAP_IP_ADDRESS, Constants.BOOTSTRAP_PORT)
+        self.blueprint = Blueprint("nodes", __name__)
+        self.nodes_counter = 1
+        self.is_bootstrapping_phase_over = False
+        # equivalent to using @self.blueprint.route on add_node
+        # (which wouldn't work because of the self prefix)
+        self.blueprint.add_url_rule("/", "nodes", self.add_node, methods=["POST"])
 
-    @staticmethod
-    @nodes_blueprint.route('/', methods=['POST'])
-    def add_node():
+    def add_node(self):
+        """
+        Method that gets called when a node sends a POST request at the /nodes endpoint
+        Adds the node's info (public key, ip, port) to the bootstraps node list and
+        returns the node's assigned id.
+        """
+
         # Mapping request body to class
         join_request = JoinRequest.from_json(request.json)
         logging.info(f"Received request to add the following node to the network: {join_request.to_dict()}.")
 
         # Performing validations
-        NodesController.perform_validations_for_post(join_request)
+        self.validate_request(join_request)
 
         # Adding node
-        id_of_new_node = NodesController.nodes_counter
-        new_node = Node.from_request(join_request, id_of_new_node)
-        NodeMemory.add_node_to_list(new_node)
-        NodesController.nodes_counter += 1
-        logging.info(f"Node with id {id_of_new_node} has been added to the network.")
-
-        if NodesController.nodes_counter > NodesController.max_number_of_nodes:
-            logging.info("Reached maximum number of nodes. Sending the final node list to all participants.")
-            NodesController.is_bootstrapping_phase_over = True
-            # TODO: Bootstrapping complete. Broadcast appropriate message to all.
+        self.bootstrap.add_node(join_request, self.nodes_counter)
+        logging.info(f"Node with id {self.nodes_counter} has been added to the network.")
 
         # Creating response
-        response = JoinResponse(id_of_new_node)
+        response = JoinResponse(self.nodes_counter)
+        self.nodes_counter += 1
+
+        # Check if every expected node has joined
+        if self.nodes_counter > Constants.MAX_NODES:
+            logging.info("Reached maximum number of nodes. Sending the final node list to all participants.")
+            self.is_bootstrapping_phase_over = True
+            # TODO: Bootstrapping complete. Broadcast appropriate message to all.
 
         return response.to_dict()
 
-    @staticmethod
-    def perform_validations_for_post(join_request: JoinRequest):
-        if not NodeMemory.is_bootstrap:
-            log_message = "Bad request: Not bootstrap node."
-            logging.warning(log_message)
-            abort(400, description=log_message)
-
-        if NodesController.is_bootstrapping_phase_over:
+    def validate_request(self, request: JoinRequest):
+        if self.is_bootstrapping_phase_over:
             log_message = "Bad request: Bootstrapping phase is over."
             logging.warning(log_message)
             abort(400, description=log_message)
 
-        if NodeMemory.node_already_in_list(join_request.ip_address, join_request.port):
+        if self.bootstrap.node_has_joined(request.ip_address, request.port):
             log_message = "Bad request: Node with given ip and port has already been added."
             logging.warning(log_message)
-            abort(400, description=log_message)
+            abort(400, description=log_message) 
 
 
+# class NodesController:
+#     """
+#         A controller containing all the endpoints related to the Node resource.
+#         Should only be used for the bootstrapping phase as the network is permanent after that.
+#     """
+#     nodes_blueprint = Blueprint('nodes', __name__)
+#     nodes_counter = 1
+#     is_bootstrapping_phase_over = False
+#     max_number_of_nodes = 10
+
+#     @staticmethod
+#     @nodes_blueprint.route('/', methods=['POST'])
+#     def add_node():
+#         # Mapping request body to class
+#         join_request = JoinRequest.from_json(request.json)
+#         logging.info(f"Received request to add the following node to the network: {join_request.to_dict()}.")
+
+#         # Performing validations
+#         NodesController.perform_validations_for_post(join_request)
+
+#         # Adding node
+#         id_of_new_node = NodesController.nodes_counter
+#         new_node = Node.from_request(join_request, id_of_new_node)
+#         NodeMemory.add_node_to_list(new_node)
+#         NodesController.nodes_counter += 1
+#         logging.info(f"Node with id {id_of_new_node} has been added to the network.")
+
+#         if NodesController.nodes_counter > NodesController.max_number_of_nodes:
+#             logging.info("Reached maximum number of nodes. Sending the final node list to all participants.")
+#             NodesController.is_bootstrapping_phase_over = True
+#             # TODO: Bootstrapping complete. Broadcast appropriate message to all.
+
+#         # Creating response
+#         response = JoinResponse(id_of_new_node)
+
+#         return response.to_dict()
--- node.py
+++ node.py
@@ -3,22 +3,18 @@ import requests
 from constants import Constants
 from request_classes.join_request import JoinRequest
 from response_classes.join_response import JoinResponse
+from wallet import Wallet
+from transaction import TransactionBuilder
 
 
 class Node:
-    def __init__(self, public_key, ip_address, port, id=None):
-        self.id = id
-        self.public_key = public_key
+    def __init__(self, ip_address, port, node_id = None):
+        self.id = node_id
         self.ip_address = ip_address
         self.port = port
-
-    @classmethod
-    def from_request(cls, join_request: JoinRequest, id: int):
-        """
-            Creates a Node object from the data of a join request.
-            Assigns to the node the id specified (locally).
-        """
-        return cls(join_request.public_key, join_request.ip_address, join_request.port, id)
+        self.wallet = Wallet()
+        self.public_key = self.wallet.public_key
+        self.tx_builder = TransactionBuilder(self.wallet)
 
     def request_to_join_network_and_get_assigned_id(self):
         """
@@ -32,9 +28,24 @@ class Node:
         # Set the headers with the appropriate Content-Type
         headers = {'Content-Type': 'application/json'}
 
-        raw_response = requests.post(Constants.get_bootstrap_node_url() + "/nodes", json=request.to_dict(),
+        raw_response = requests.post(Constants.get_bootstrap_node_url() + "/nodes",
+                                     json=request.to_dict(),
                                      headers=headers)
         response = JoinResponse.from_json(raw_response.json())
 
         # Assign returned id to node
         self.id = response.id
+
+class Bootstrap:
+    def __init__(self, ip_address, port):
+        self.node = Node(ip_address, port, Constants.BOOTSTRAP_ID)
+        self.other_nodes = {}
+
+    def add_node(self, request: JoinRequest, id: int):
+        self.other_nodes[id] = (request.ip_address, request.port, request.public_key)
+
+    def node_has_joined(self, ip_address, port):
+        for node_ip, node_port, _ in self.other_nodes.values():
+            if node_ip == ip_address and node_port == port:
+                return True
+        return False
