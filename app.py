b577e289c0ceb44e07673ea41390a631d04811c1
Lazaros Athanasiadis <lathanasiadis.dsml@gmail.com>
2024-02-21 20:10:52 +0200

    set up argparse, dynamically find IP

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 import logging
-
+import argparse
 from flask import Flask
 from flask_restful import Api
-
 from controllers.bootstrap_controller import BootstrapController
 from node import Node, Bootstrap
 from constants import Constants
+from helper import myIP
 
-is_bootstrap_str = input("Start node as bootstrap node? (y/n): ").lower()
-is_bootstrap = is_bootstrap_str == 'y'
+parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--bootstrap", action = argparse.BooleanOptionalAction, default = False)
+parser.add_argument("-p", "--port", nargs = "?", const = "8000", default = "8000")
+args = parser.parse_args()
+
+is_bootstrap = args.bootstrap
+
+print("-----------------------------------------------------------")
+print("""
+ ____  _            _        _           _   
+|  _ \| |          | |      | |         | |  
+| |_) | | ___   ___| | _____| |__   __ _| |_ 
+|  _ <| |/ _ \ / __| |/ / __| '_ \ / _` | __|
+| |_) | | (_) | (__|   < (__| | | | (_| | |_ 
+|____/|_|\___/ \___|_|\_\___|_| |_|\__,_|\__|
+""")
+print("-----------------------------------------------------------")
 
 if is_bootstrap:
     app = Flask(__name__)
@@ -22,22 +37,12 @@ if is_bootstrap:
     app.register_blueprint(bootstrap.blueprint, url_prefix='/nodes')
     
     # Run the API
-    print("-----------------------------------------------------------")
-    print("""
-    ____  _            _        _           _   
-    |  _ \| |          | |      | |         | |  
-    | |_) | | ___   ___| | _____| |__   __ _| |_ 
-    |  _ <| |/ _ \ / __| |/ / __| '_ \ / _` | __|
-    | |_) | | (_) | (__|   < (__| | | | (_| | |_ 
-    |____/|_|\___/ \___|_|\_\___|_| |_|\__,_|\__|
-    """)
-    print("-----------------------------------------------------------")
+
     print(f"Bootstrap has joined the network.")
-    app.run(port=Constants.BOOTSTRAP_PORT)
+    app.run(host="0.0.0.0", port=Constants.BOOTSTRAP_PORT)
 else:
-    ip_address = "http://localhost"  # TODO: Replace this maybe. Find it automatically?
-    port = int(input("Enter port number: "))
-    node = Node(ip_address, port)
+    ip_address = myIP() # TODO: Replace this maybe. Find it automatically?
+    node = Node(ip_address, args.port)
     node.request_to_join_network_and_get_assigned_id()
     # TODO: parse reply and do node.id = reply.id
     # TODO: node also listens on endpoints
