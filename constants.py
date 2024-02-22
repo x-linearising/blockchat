0c169d398f42c2aa2abef20b72765b40cc166318
Lazaros Athanasiadis <lathanasiadis.dsml@gmail.com>
2024-02-22 21:59:25 +0200

    bring back NodeInfo, other small PR requested changes

--- constants.py
+++ constants.py
@@ -4,6 +4,8 @@ class Constants:
     BOOTSTRAP_ID = 0
     BOOTSTRAP_PUBLIC_KEY = "dummy key"  # TODO: Define boostrap public key. It should not be generated on runtime.
     MAX_NODES = 10
+    JSON_HEADER = {'Content-Type': 'application/json'}
+    CAPACITY = 5
     @classmethod
     def get_bootstrap_node_url(cls):
         url = f"{cls.BOOTSTRAP_IP_ADDRESS}:{cls.BOOTSTRAP_PORT}"
