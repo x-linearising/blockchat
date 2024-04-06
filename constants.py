a78ee9160a52723e0b86c89c187719cd98e69c36
i.katsantas <i.katsantas@xe.gr>
2024-04-06 13:12:38 +0300

    Capacity 20

--- constants.py
+++ constants.py
@@ -13,7 +13,7 @@ class Constants:
 
     MAX_NODES = 10
     JSON_HEADER = {'Content-Type': 'application/json'}
-    CAPACITY = 10
+    CAPACITY = 20
     STARTING_BCC_PER_NODE = 1000
     TRANSFER_FEE_MULTIPLIER = 1.03
     INITIAL_STAKE = 10
